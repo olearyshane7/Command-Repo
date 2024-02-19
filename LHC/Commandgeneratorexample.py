dea47fe70b4b88605f6c753cb4a88420b0e17326
Daz <74672770+olearyshane7@users.noreply.github.com>
2024-02-19 16:34:52 -0500

    errorlogging fix

--- LHC/Commandgeneratorexample.py
+++ LHC/Commandgeneratorexample.py
@@ -3,13 +3,8 @@ from tkinter import messagebox
 import pyperclip
 import random
 import logging
-
-
-class InvalidStateException(Exception):
-    pass
-
-class InvalidIMEIException(Exception):
-    pass
+from ErrorLogging import ErrorLogging
+from ErrorLogging import InvalidStateException
 
 class IMEICommandGenerator:
     def __init__(self, master):
@@ -138,66 +133,10 @@ main"""
 
         # Configure the logging
         logging.basicConfig(filename=self.error_log_filepath, level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-
-        # Array of randomly generated error messages
-        # Commented error messages are passive aggressive statements. 
-        # Only activate commented error messages if you have a strong heart and mind.
-
-        # IMEI is blank
-        self.error_messages = [
-            # "I can't even with you right now.",
-            # "Do you not know how to copy and paste properly?",
-            # "Arthas did nothing wrong",
-            # "You're wasting your time. Populate the IMEI properly.",
-            # "What is your IQ?",
-            # "Just have somebody else swap for you.."
-            "It's Blank.",
-            "Please, Populate the field",
-            "You forgot to type in the IMEI",
-            "Boss Ahead",
-            "NANI???"
-        ]
         
-        # IMEI is short
-        self.short_error_messages = [
-            # "I'm being as lazy as you are with these error messages",
-            # "Did you not learn how to use a computer as a child?",
-            # "Are you blind?",
-            # "Can you do anything right?",
-            # "What came first, the messed up IMEI command or the failed SIM Swap?"
-            "The Etruscan shrew is the smallest mammal on earth, much like the IMEI input",
-            "The IMEI is too short.",
-            "Try again, it's too short",
-            "Generally the playoff run of the Toronto Maple Leafs."
-        ]
-
-        # IMEI is long
-        self.long_error_messages = [
-            # "Your power level is clearly under 9000.",
-            # "Fact: That input was trash."
-            "The IMEI is too long.",
-            "Try again, it's too long.",
-            "It's an IMEI number not a python. Shorten it to 15 integers",
-            "I always got yelled at for keeping the fridge open too long."
-        ]
-
-        # IMEI has a letter
-        self.letter_error_messages = [
-            # "Failure ahead...",
-            # "It's alright. It's been a long day.",
-            # "If you input the IMEI properly next time, you'll successfully complete the sim swap.",
-            # "Did you input it correctly?",
-            # "Try again!"
-            "Check for letters.",
-            "The IMEI can only be integers",
-            "Please delete any letters"
-           
-        ]
-        
-
         # IMEI code empty error
         if not imei:
-            self.show_random_error()
+            self.show_random_error(self)
             logging.debug(f"An error occurred: Blank box")
             return  
         
@@ -244,24 +183,24 @@ main"""
         messagebox.showinfo("IMEI Command", f"IMEI command copied to clipboard:\n\n{initial_command}")
 
     def show_random_error(self):
-        random_error = random.choice(self.error_messages)      
+        random_error = random.choice(ErrorLogging.error_messages)      
         messagebox.showerror("Error", random_error)
 
     # IMEI Short Error messages
     def show_random_short_error(self):
-        random_error = random.choice(self.short_error_messages)
+        random_error = random.choice(ErrorLogging.short_error_messages)
         messagebox.showerror("Error", random_error)
          
     
     # IMEI Long Error messages
     def show_random_long_error(self):
-        random_error = random.choice(self.long_error_messages)
+        random_error = random.choice(ErrorLogging.long_error_messages)
         messagebox.showerror("Error", random_error)
          
 
     # IMEI String error messages
     def show_random_letter_error(self):
-        random_error = random.choice(self.letter_error_messages)
+        random_error = random.choice(ErrorLogging.letter_error_messages)
         messagebox.showerror("Error", random_error)
 
 # Create the main window

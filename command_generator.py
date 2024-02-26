f191329526633202c616679b7e23a004da3851d3
Daz <74672770+olearyshane7@users.noreply.github.com>
2024-02-26 09:44:03 -0500

    errorlogs

--- command_generator.py
+++ command_generator.py
@@ -2,6 +2,7 @@ import pyperclip
 import logging
 from tkinter import messagebox
 import random
+from errorlogs import ErrorLogging
 
 class InvalidStateException(Exception):
     pass
@@ -21,82 +22,27 @@ def generate_command(self, action):
     # Configure the logging
     logging.basicConfig(filename=self.error_log_filepath, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-    # Array of randomly generated error messages
-    # Commented error messages are passive aggressive statements.
-    # Only activate commented error messages if you have a strong heart and mind.
-
-    # IMEI is blank
-    error_messages = [
-        # "I can't even with you right now.",
-        # "Do you not know how to copy and paste properly?",
-        # "Arthas did nothing wrong",
-        # "You're wasting your time. Populate the IMEI properly.",
-        # "What is your IQ?",
-        # "Just have somebody else swap for you.."
-        "It's Blank.",
-        "Please, Populate the field",
-        "You forgot to type in the IMEI",
-        "Boss Ahead",
-        "NANI???"
-    ]
-
-    # IMEI is short
-    short_error_messages = [
-        # "I'm being as lazy as you are with these error messages",
-        # "Did you not learn how to use a computer as a child?",
-        # "Are you blind?",
-        # "Can you do anything right?",
-        # "What came first, the messed up IMEI command or the failed SIM Swap?"
-        "The Etruscan shrew is the smallest mammal on earth, much like the IMEI input",
-        "The IMEI is too short.",
-        "Try again, it's too short",
-        "Generally the playoff run of the Toronto Maple Leafs."
-    ]
-
-    # IMEI is long
-    long_error_messages = [
-        # "Your power level is clearly under 9000.",
-        # "Fact: That input was trash."
-        "The IMEI is too long.",
-        "Try again, it's too long.",
-        "It's an IMEI number not a python. Shorten it to 15 integers",
-        "I always got yelled at for keeping the fridge open too long."
-    ]
-
-    # IMEI has a letter
-    letter_error_messages = [
-        # "Failure ahead...",
-        # "It's alright. It's been a long day.",
-        # "If you input the IMEI properly next time, you'll successfully complete the sim swap.",
-        # "Did you input it correctly?",
-        # "Try again!"
-        "Check for letters.",
-        "The IMEI can only be integers",
-        "Please delete any letters"
-
-    ]
-
     # IMEI code empty error
     if not imei:
-        show_random_error(error_messages)
+        show_random_error(ErrorLogging.error_messages)
         logging.debug(f"An error occurred: Blank box")
         return
 
     # IMEI code too short
     if len(imei) < 15:
-        show_random_short_error(short_error_messages)
+        show_random_short_error(ErrorLogging.short_error_messages)
         logging.debug(f"An error occurred: IMEI too short")
         return
 
     # IMEI code is too long
     if len(imei) > 15:
-        show_random_long_error(long_error_messages)
+        show_random_long_error(ErrorLogging.long_error_messages)
         logging.debug(f"An error occurred: IMEI too long")
         return
 
     # IMEI code has a letter in it
     if not imei.isdigit():
-        show_random_letter_error(letter_error_messages)
+        show_random_letter_error(ErrorLogging.letter_error_messages)
         logging.debug(f"An error occurred: Includes a letter.")
         return
 

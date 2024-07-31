243239d62b30099a577a2c1273bc5985c87acf31
Daz <74672770+olearyshane7@users.noreply.github.com>
2024-07-31 15:44:24 -0400

    10.15

--- myapp.py
+++ myapp.py
@@ -6,9 +6,8 @@ import logging
 from tkinter.constants import END
 # command_generator.py
 from IMEICommandGenerator import IMEICommandGenerator
-from FortiGateCheatSheetApp import FortiGateCheatSheetApp
 
-# def lhc_button_click():
+# def config_button_click():
 
 root = tk.Tk()
 app = IMEICommandGenerator(root)
@@ -16,9 +15,8 @@ root.mainloop()
 
 # def fg_button_click():
 
-#      # Move the relevant code here
+#      # Move the relevant code here 	USPSGRT132209997SYRCNY-SW
 #      root = tk.Tk()
-#      app = FortiGateCheatSheetApp(root)
 #      root.mainloop()    
 # Create the main window
 # root = tk.Tk()
@@ -26,12 +24,9 @@ root.mainloop()
 # Increase the window size
 # root.geometry("200x200")
 
-# Create a button with the label "lhc" that calls the lhc_button_click function when clicked
-# lhc_button = ttk.Button(root, text="lhc", command=lhc_button_click)
-# lhc_button.pack()
-
-# fg_button = ttk.Button(root, text="fortigate", command=fg_button_click)
-# fg_button.pack()
+# Create a button with the label "config" that calls the config_button_click function when clicked
+# config_button = ttk.Button(root, text="config", command=config_button_click)
+# config_button.pack()
 
 # Run the Tkinter event loop
 # root.mainloop()
